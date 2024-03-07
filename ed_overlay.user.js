1975b38e78a96566ceb041a009a914010438ed23
Stefvron <stefvron@steffenmolitor.de>
2024-03-07 05:58:31 +0100

    Fixed URLs

--- ed_overlay.user.js
+++ ed_overlay.user.js
@@ -9,8 +9,8 @@
 // @website     https://github.com/Stefvron/ed_overlay
 // @match       https://everyonedraw.com/*
 // @run-at      document-idle
-// @updateURL   https://stefvron.github.io/ed_overlay/raw/master/ed_overlay.user.js
-// @downloadURL https://stefvron.github.io/ed_overlay/raw/master/ed_overlay.user.js
+// @updateURL   https://github.com/Stefvron/ed_overlay/raw/master/ed_overlay.user.js
+// @downloadURL https://github.com/Stefvron/ed_overlay/raw/master/ed_overlay.user.js
 // ==/UserScript==
 
 function GM_addStyle (cssStr) {
